a3d723a231e88ab0e31ab8163ea89d2475eaf521
zheng <765324639@qq.com>
2019-12-22 20:00:53 +0800

    dubbo2.7 provider端

--- dubbo-demo-provider/src/main/java/demo/provider/DemoServiceImpl.java
+++ dubbo-demo-provider/src/main/java/demo/provider/DemoServiceImpl.java
@@ -4,6 +4,7 @@ import demo.service.IDemoService;
 
 public class DemoServiceImpl implements IDemoService {
 
+    @Override
     public String sayHello(String name) {
         return "Hello " + name;
     }
